26760fcca32e039d5b0b5884a581141f5abc6e78
sunanth-samala <sunanth.samala7@email.com>
2024-02-08 11:52:48 +0530

    npm

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContexProvider } from "./context/authContext";
+import Register from "./pages/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContexProvider>
-      <App />
+      <Register/>
     </AuthContexProvider>
   </React.StrictMode>
 );
