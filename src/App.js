d20b29d5471be326660e3a03f539594c0d59fcd2
sunanth-samala <sunanth.samala7@email.com>
2024-02-08 11:55:31 +0530

    n

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-} from "react-router-dom";
+
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
@@ -13,50 +8,12 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./style.scss"
 
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/post/:id",
-        element: <Single />,
-      },
-      {
-        path: "/write",
-        element: <Write />,
-      },
-    ],
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 
 function App() {
   return (
     <div className="app">
       <div className="container">
-        <RouterProvider router={router} />
+        
       </div>
     </div>
   );
